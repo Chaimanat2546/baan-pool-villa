7df2eeb7bccbed78e667a6f8cf2fc1527a19ef0b
Chaimanat <Chaymanus2003@gmail.com>
2026-05-25 18:17:46 +0700

    chore: configure villa app baseline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "devillegroups.com",
+        pathname: "/imgs/profile_imgs_large/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.devillegroups.com",
+        pathname: "/imgs/profile_imgs_large/**",
+      },
+      {
+        protocol: "https",
+        hostname: "rqizfiayvcbozlzuvbok.supabase.co",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
