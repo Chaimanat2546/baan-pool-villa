52dcf8e9592d63617409e1b4f2ec033120715883
Chaimanat <Chaymanus2003@gmail.com>
2026-06-04 18:26:40 +0700

    perf(fonts): reduce Prompt weights

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const prompt = Prompt({
   preload: false,
   variable: "--font-prompt",
   subsets: ["latin", "thai"],
-  weight: ["400", "500", "600", "700", "800", "900"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
