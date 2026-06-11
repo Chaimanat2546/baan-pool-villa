72b77b6afb2a6d3a6c3561dd16402a84d4683226
Chaimanat <Chaymanus2003@gmail.com>
2026-06-11 13:23:37 +0700

    feat: add Permissions-Policy header to enhance security configuration

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,10 @@ const securityHeaders = [
     key: "Referrer-Policy",
     value: "strict-origin-when-cross-origin",
   },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  },
 ];
 
 const nextConfig: NextConfig = {
